566e1cfc99941da6c8fcf59f28242b8e795d4a0e
Darakuu <elviost@gmail.com>
2026-05-15 16:52:48 +0200

    chore: specify that scripts are debug only

--- scripts/consume_raw_topic.ps1
+++ scripts/consume_raw_topic.ps1
@@ -2,3 +2,6 @@ docker compose exec kafka /opt/kafka/bin/kafka-console-consumer.sh `
   --bootstrap-server localhost:9092 `
   --topic cicd.otel.raw `
   --from-beginning
+
+
+# this script is for debug purposes only, you don't need to run it for the project to function correctly.
